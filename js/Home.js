1c90f6183fde994e353991f11b11ce74d08f1a69
Leslie Reyes <ldianereyes@gmail.com>
2016-01-16 22:45:25 -0500

    fix home and about

--- js/Home.js
+++ js/Home.js
@@ -1,3 +1,3 @@
 console.log("Home.js");
 
-$('nav').css('background-color', 'rgb(255,255,255)').css('box-shadow', 'none');
+//$('nav').css('background-color', '$background-color').css('box-shadow', 'none');
